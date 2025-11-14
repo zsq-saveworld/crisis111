204baf360952474c0e52b71dfb54702730c77c38
willful <zsq040926@163.com>
2025-11-14 11:15:54 +0800

    初始4

---
+++ src/main/java/com/jvlei/datadispatch/mapper/ExamPredictMapper.java
@@ -0,0 +1,7 @@
+package com.jvlei.datadispatch.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jvlei.datadispatch.model.entity.ExamPredictResult;
+
+public interface ExamPredictResultMapper extends BaseMapper<ExamPredictResult> {
+}
